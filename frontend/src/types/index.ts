92b0ff9f3eea8740eb20d8e2a904c714bfad829c
Gaurav kumar <116382282+Teletetra@users.noreply.github.com>
2026-08-23 00:09:11 +0530

    Export frontend types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,7 @@
+export * from './auth.types';
+export * from './chat.types';
+export * from './user.types';
+export * from './bluetooth.types';
+export * from './file.types';
+export * from './notification.types';
+export * from './socket.types';
