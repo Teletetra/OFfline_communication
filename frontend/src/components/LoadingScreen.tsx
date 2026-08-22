64820178ab29699fcf0773701d4af359c04456c8
Gaurav kumar <116382282+Teletetra@users.noreply.github.com>
2026-08-22 23:47:08 +0530

    Add loading screen

---
+++ frontend/src/components/LoadingScreen.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const LoadingScreen: React.FC = () => <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center' }}><div>Loading…</div></div>;
+export default LoadingScreen;
