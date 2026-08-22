c90e557f8592d82c4dad17c80daf8a8bbf339bb8
Gaurav kumar <116382282+Teletetra@users.noreply.github.com>
2026-08-23 00:11:41 +0530

    Export route components

---
+++ frontend/src/routes/index.ts
@@ -0,0 +1,4 @@
+export {default as AppRoutes} from './AppRoutes';
+export {default as AuthRoutes} from './AuthRoutes';
+export {default as ProtectedRoute} from './ProtectedRoute';
+export {default as PublicRoute} from './PublicRoute';
